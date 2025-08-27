7daf78188ccd0e04ea091dcf1efba4aaa87493c5
FIGHB <li839521927@gmail.com>
2025-08-27 23:36:26 +0800

    sliding menu finish

--- SMALL-WATCH-PROJECT-STM32F103C8T6/User/self_menu.c
+++ SMALL-WATCH-PROJECT-STM32F103C8T6/User/self_menu.c
@@ -2,8 +2,8 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-08-15 20:40:53
  * @LastEditors: FIGHB li839521927@gmail.com
- * @LastEditTime: 2025-08-27 22:56:12
- * @FilePath: \PROJECT-SMALL-WATCH\SMALL-WATCH-PROJECT-STM32F103C8T6\User\self_menu.c
+ * @LastEditTime: 2025-08-27 23:32:09
+ * @FilePath: \SMALL-WATCH-PROJECT-STM32F103C8T6\User\self_menu.c
  * @Description:
  *
  * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved.
